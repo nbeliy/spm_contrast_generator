50d2a7557e27d85b1425e011a7d1bd2228112e45
Beliy Nikita <beliy.nikita@uliege.be>
2024-11-05 10:54:54 +0100

    Refactored generate_session_name

--- generate_session_name.m
+++ generate_session_name.m
@@ -2,6 +2,11 @@ function res = generate_session_name(session_id)
   % Generates session string as in intable from session id as in SPM file
   % For ex: 'Sn(1)' --> 'T0'
   % Need to be changed if session naming is changed
+
+  % This retrieve the session number from Sn(X) string
   tok = regexp(session_id, 'Sn\(([0-9]+)\)', 'tokens');
-  res = ['T', num2str(str2double(tok{1}{1}) - 1)];
+  ses_num = str2double(tok{1}{1});
+
+  % This transforms session number to Session name
+  res = sprintf('T%d', ses_num - 1);
 end
